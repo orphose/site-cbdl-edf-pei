2f99d5744dfde6e4d861bdd85d96a474f2e79b02
Pierre FOUREL <start@orphose.studio>
2025-12-16 20:18:39 -0300

    refactor: supprimer 9 composants redondants non utilisés

--- src/components/centrale/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Export des composants de la page Centrale
- */
-
-export { default as CentraleHeroSection } from "./CentraleHeroSection";
-export { default as WhySection } from "./WhySection";
-export { default as DescriptionSection } from "./DescriptionSection";
-export { default as FootprintSection } from "./FootprintSection";
-
